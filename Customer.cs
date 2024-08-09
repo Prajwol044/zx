afa02509274f57d44bd927eb1c9ad596ca062eb0
Prajwol044 <prajwolbashyal44@gmail.com>
2024-08-09 15:43:49 +1000

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4Ex3
+{
+    internal class Customer
+    {
+        int id { get; set; }
+        string name { get; set; }
+         
+        public Customer(int i, string n)
+        {
+            id = i;
+            name = n;
+        }
+
+        }
+    }
+
